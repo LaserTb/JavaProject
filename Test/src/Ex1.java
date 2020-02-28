d0ac8cae3c7d048a288f9bd5a19bee1d7d8f9541
AMAJ203 <AMAJ203@PC13142469.cc-college.gecs.loc>
2020-02-28 13:34:57 +0100

    ..

--- Test/src/Ex1.java
+++ Test/src/Ex1.java
@@ -40,19 +40,55 @@
 //} 
 
 
+//class Ex1 { 
+//	public static void main(String[] args) 
+//	{ 
+//		int marks = 90; 
+//		String result =        marks > 60 ? "Pass with 1st div."
+//				           :   marks < 50 ? "Pass with 2nd div." 
+//
+//						//						:	marks < 40 ? "Pass with 3nd div.";
+//						//								Compile Time Erroe 
+//						//								Correction 
+//						   :    marks < 40 ? "Pass with 3nd div."
+//							:"Fail";
+//		System.out.println(result); 
+//	} 
+//} 
+
+
+//class Ex1 { 
+//	public static void main(String[] args) 
+//	{ 
+//		String ta = "A "; 
+//		ta = ta.concat("B "); 
+//		String tb = "C "; 
+//		ta = ta.concat(tb); 
+//		ta.replace('C', 'D'); 
+//		ta = ta.concat(tb); 
+//		System.out.println(ta);
+//		String name;
+//		
+//		  
+//	} 
+//} 
+
 class Ex1 { 
 	public static void main(String[] args) 
 	{ 
-		int marks = 90; 
-		String result =        marks > 60 ? "Pass with 1st div."
-				           :   marks < 50 ? "Pass with 2nd div." 
+		int a = 10; 
+		int b = 20; 
 
-						//						:	marks < 40 ? "Pass with 3nd div.";
-						//								Compile Time Erroe 
-						//								Correction 
-						   :    marks < 40 ? "Pass with 3nd div."
-							:"Fail";
-		System.out.println(result); 
+		if (++a <= 10 && --b < 20) {} 
+		System.out.println("Output of && operator: "
+						+ "a = " + a + " b = " + b); 
+		System.out.println("-------------"); 
+
+		a = 10; 
+		b = 20; 
+		if (++a <= 10 & --b < 20) {} 
+		System.out.println("Output of & operator: "
+						+ "a = " + a + " b = " + b); 
 	} 
 } 
 
