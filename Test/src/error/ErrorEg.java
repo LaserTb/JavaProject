5666fee4bb7053c240d0d3c0eea33fe26091595e
AMAJ203 <AMAJ203@PC13142469.cc-college.gecs.loc>
2020-03-03 10:35:39 +0100

    ..

---
+++ Test/src/error/ErrorEg.java
@@ -0,0 +1,26 @@
+package error;
+
+//Java program illustrating stack overflow error 
+//by doing infinite recursion 
+
+class StackOverflow { 
+	public static void test(int i) 
+	{ 
+		// No correct as base condition leads to 
+		// non-stop recursion. 
+		if (i == 0) 
+			return; 
+		else { 
+			test(i++); 
+		} 
+	} 
+} 
+public class ErrorEg { 
+
+	public static void main(String[] args) 
+	{ 
+
+		// eg of StackOverflowError 
+		StackOverflow.test(5); 
+	} 
+} 
--- Test/src/testmain/Main.java
+++ Test/src/testmain/Main.java
@@ -31,41 +31,48 @@ package testmain;
 
 //Which of the following statements are correct i
 //f they replace the comment line? 
- public class Main{ 
-//       public int i; 
-       public static void main(String argv[]){ 
+// public class Main{ 
+////       public int i; 
+//       public static void main(String argv[]){ 
 //          Main sc = new Main(); 
 //          // Comment line 
 //          
 ////          System.out.println(i);
 //          System.out.println(sc.i);
 ////          System.out.println(Main.i);
-//          System.out.println((new Main()).i);
-
-          
+//          System.out.println((new Main()).i);  
 //          System.out.println( 16  >>>  3); //16= 10000 to 2=10
 //          System.out.println(8<<2);// 8=1000 << to 32=100000
-    	   
-    	   
-    	   
-//    	  it will be printed out when the following code is executed 
-//    	   ******default and two*****
-    	   switch (5) { 
-    	    case 0: 
-    	       System.out.println("zero"); 
-    	       break; 
-    	    case 1: 
-    	       System.out.println("one"); 
-    	    default: 
-    	       System.out.println("default"); 
-    	    case 2: 
-    	       System.out.println("two"); 
-    	       break;
-    	} 
-       } 
-     } 
+//    	   
+//    	   
+//    	   
+
+//    	   
+//    	} 
+//       } 
+//     } 
  
 
+
+
+////it will be printed out when the following code is executed 
+////******default and two*****
+// public class Main {
+//	  public static void main(String[] args) {
+//	    switch (5) {// w  w w.  j  a  v  a 2  s .  c  o  m
+//	    case 0:
+//	      System.out.println("zero");
+//	      break;
+//	    case 1:
+//	      System.out.println("one");
+//	    default:
+//	      System.out.println("default");
+//	    case 2:
+//	      System.out.println("two");
+//	      break;
+//	    }
+//	  }
+//	}
 
 // public class Main {
 //  public static void main(String[] args) {
@@ -81,3 +88,47 @@ package testmain;
 //    } 
 //  }
 //}
+
+
+
+
+//public class Main { 
+//    public static int getVal() { 
+//        return 100; 
+//    } 
+//    public static void main(String args[]) { 
+//         int num = 10; 
+//        final int num2 = 20; 
+//        switch (num=10*3) { 
+//            // INSERT CODE HERE 
+//
+//        case 10*3: System.out.println(2);
+////        case num: System.out.println(3);
+//        case 10/3: System.out.println(4);
+//        case num2: System.out.println(5);
+//
+//            break; 
+//            default: System.out.println("default"); 
+//        } 
+//    } 
+//} 
+
+
+
+////Compilation error 
+////10*2-20 is 0, which is duplicated with another 0 in the case statement.
+//public class Main {
+//	  public static void main(String args[]) {
+//	    int num = 0;
+//
+//	    switch (num) {
+//	    default:
+//	      System.out.println("default");
+//	    case 0:
+//	      System.out.println("case1");
+////	    case 10 * 2 - 20:
+////	      System.out.println("case2");
+////	      break;
+//	    }
+//	  }
+//	}
