ae9ae20894286fd1bc6af6161354cb59d386fa5d
AMAJ203 <AMAJ203@PC13142469.cc-college.gecs.loc>
2020-03-03 13:16:36 +0100

    ..

--- Test/src/DateTimeTest/DemoLocakDate.java
+++ Test/src/DateTimeTest/DemoLocakDate.java
@@ -17,13 +17,35 @@ public class DemoLocakDate {
 		LocalDateTime dt1= LocalDateTime.of(da1, ti1);
 		System.out.println(dt1);
 		
-		
+		System.out.println("********************");
 		
 		LocalDate date =  LocalDate.of(2010, Month.JANUARY, 10);
+		System.out.println(date);
+		date = date.minusDays(5);
+		System.out.println(date);
+		date = date.plusDays(20);
+		System.out.println(date);
+		date = date.plusMonths(15);
+		System.out.println(date);
+		date =date.plusYears(4);
+		System.out.println(date);
 		
-				
 		
-
+		System.out.println("+++++++++++++++++++");
+		
+		LocalDate datet1 =  LocalDate.of(2010, Month.JANUARY, 10);
+		LocalTime time1 =LocalTime.of(12, 12);
+		LocalDateTime datetime= LocalDateTime.of(datet1, time1);
+		System.out.println(datetime);
+		datetime = datetime.minusHours(2);
+		System.out.println(datetime);
+		datetime = datetime.plusMinutes(300);
+		System.out.println(datetime);
+		datetime = datetime.plusMonths(15);
+		System.out.println(datetime);
+		datetime =datetime.plusSeconds(2000000);
+		System.out.println(datetime);
+		
 	}
 
 }
